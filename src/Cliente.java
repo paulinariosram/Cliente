4cada17b5fedb1ae21fa2d40db553d790b366226
arnucagu1 <aacastro@sura.com.co>
2022-01-20 17:04:48 -0500

    AC relación de  tipo y número de documento en la clase

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,7 +1,18 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
 
 public class Cliente {
 
 	public static void main (String [] args) {
+		
+	
+		
+		String tipoDocumento = (JOptionPane.showInputDialog("Ingrese tipo de documento"));
+		int numeroDeDocumento = Integer.parseInt(JOptionPane.showInputDialog("Ingrese numero de documento"));
+		
+		JOptionPane.showMessageDialog(null, "El tipo y número de documento es : " +(tipoDocumento +" " +numeroDeDocumento));
+		
 		 
 	}
 }
